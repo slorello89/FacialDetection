9a14a4d89e722868d6c5bd454adb630dcc089514
slorello89 <slorello89@gmail.com>
2020-05-04 15:30:45 -0400

    first commit

---
+++ FacialDetection/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Emgu.CV;
+using System.IO;
+
+namespace FacialDetection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var faceClassifier = new CascadeClassifier(Path.Join("resources", 
+                "haarcascade_frontalface_default.xml"));
+            var img = CvInvoke.Imread(Path.Join("resources", "imageWithFace.jpg"));
+
+            var faces = faceClassifier.DetectMultiScale(img,
+                minSize: new System.Drawing.Size(300,300));
+            foreach(var face in faces)
+            {
+                CvInvoke.Rectangle(img, face, 
+                    new Emgu.CV.Structure.MCvScalar(255, 0, 0), 10);
+            }
+
+            CvInvoke.Imshow("Faces", img);
+            CvInvoke.WaitKey(0);
+        }
+    }
+}
